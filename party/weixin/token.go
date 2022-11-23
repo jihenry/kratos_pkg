4c2e2a64c804d00aceb0699ad073317c284e64f4
jackniu <jackniu@yeahka.com>
2022-11-23 17:28:02 +0800

    update access token get

--- party/weixin/token.go
+++ party/weixin/token.go
@@ -3,11 +3,9 @@ package weixin
 import (
 	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/transport/http"
 	"gitlab.yeahka.com/gaas/pkg/util"
 )
 
@@ -22,21 +20,14 @@ func tokenSaleWellRefresh(ctx context.Context, in *RefreshTokenRequest) (*Salewe
 	data["sign"] = genYHQOpenSign(data, "kM2xtPkDFeT6qxyO")
 	reply := &SalewellTokenReply{}
 	addr := "https://oauth.salewell.com/wechat/open/app/getToken"
-	client, err := http.NewClient(context.Background(), http.WithEndpoint("https://oauth.salewell.com"))
+	err := get(addr, data, reply)
 	if err != nil {
-		return reply.Data, err
+		return nil, err
 	}
-
-	params := make([]string, 0, len(data))
-	for k, v := range data {
-		params = append(params, fmt.Sprintf("%s=%v", k, v))
+	log.Info("token refresh:", addr, reply, err)
+	if reply.Code != 0 {
+		return nil, fmt.Errorf("token get fail")
 	}
-
-	err = client.Invoke(context.Background(), "GET", "/wechat/open/app/getToken?"+strings.Join(params, "&"), nil, &reply)
-	if err != nil {
-		return reply.Data, err
-	}
-
 	log.Info("token refresh:", addr, reply, err)
 	if reply.Code != 0 {
 		return nil, fmt.Errorf("token get fail")
