1ee56c69393f1867a88f6e72b244fe907b7721a0
wdoeasy <wdoeasy@126.com>
2022-12-01 16:45:51 +0800

    添加指定时间零点

--- util/time.go
+++ util/time.go
@@ -2,7 +2,12 @@ package util
 
 import "time"
 
-func GetZeroTime() time.Time {
+func GetTodayZeroTime() time.Time {
 	year, month, day := time.Now().Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+}
+
+func GetDayZeroTime(day int) time.Time {
+	zero := GetTodayZeroTime()
+	return zero.AddDate(0, 0, day)
 }
