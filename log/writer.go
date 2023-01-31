1428640b08aa801754a24ca2392720b260f50632
himawari <1156544355@qq.com>
2023-01-31 16:26:28 +0800

    🎨 删除时间目录

--- log/writer.go
+++ log/writer.go
@@ -10,16 +10,9 @@ import (
 )
 
 func createFileWriter(dir, fileName string, maxAge int) (io.Writer, error) {
-	month := time.Now().Format("200601")
-	filePath := filepath.Join(dir, month)
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		err = os.MkdirAll(filePath, 0755)
-		if err != nil {
-			return nil, err
-		}
-	}
+	_ = os.MkdirAll(dir, 0755)
 	return rotatelogs.New(
-		filepath.Join(filePath, fileName+".%Y%m%d"+".log"),
+		filepath.Join(dir, fileName+".%Y%m%d"+".log"),
 		rotatelogs.WithLinkName(filepath.Join(dir, fileName+".log")),
 		rotatelogs.WithMaxAge(time.Hour*24*time.Duration(maxAge)),
 		rotatelogs.WithRotationTime(time.Hour*24),
