495e94464794305f225f5fdc249405c7bd9990c7
robinluo <robinluo@yeahka.com>
2023-02-07 16:05:06 +0800

    #feature 添加使用agent上报的方式

--- util/trace.go
+++ util/trace.go
@@ -30,3 +30,26 @@ func InitTracerProvider(url string, serviceName string) error {
 	otel.SetTracerProvider(tp)
 	return nil
 }
+
+
+func InitTracerProviderUseAgent(host, port string, serviceName string)error{
+	// 创建 Jaeger exporter
+	exp, err := jaeger.New(jaeger.WithAgentEndpoint(jaeger.WithAgentHost(host), jaeger.WithAgentPort(port)))
+	if err != nil {
+		return err
+	}
+	tp := tracesdk.NewTracerProvider(
+		// 将基于父span的采样率设置为100%
+		tracesdk.WithSampler(tracesdk.ParentBased(tracesdk.TraceIDRatioBased(1.0))),
+		// 始终确保再生成中批量处理
+		tracesdk.WithBatcher(exp),
+		// 在资源中记录有关此应用程序的信息
+		tracesdk.WithResource(resource.NewSchemaless(
+			semconv.ServiceNameKey.String(serviceName),
+			attribute.String("exporter", "jaeger"),
+			attribute.Float64("float", 312.23),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return nil
+}
