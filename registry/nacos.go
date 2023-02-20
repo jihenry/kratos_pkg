fc5ca2a20d7731ae6fb8e33958c03f4db4080029
wdoeasy <wdoeasy@126.com>
2023-02-20 17:07:15 +0800

    nacos增加默认值

--- registry/nacos.go
+++ registry/nacos.go
@@ -1,9 +1,12 @@
 package registry
 
 import (
+	"os"
+
 	"github.com/go-kratos/kratos/contrib/registry/nacos/v2"
 	"github.com/nacos-group/nacos-sdk-go/clients"
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/common/file"
 	"github.com/nacos-group/nacos-sdk-go/vo"
 )
 
@@ -11,7 +14,22 @@ func NewNacosClient(addr string, port uint64, copts ...constant.ClientOption) (*
 	sc := []constant.ServerConfig{
 		*constant.NewServerConfig(addr, port),
 	}
-	cc := constant.NewClientConfig(copts...)
+	cc := &constant.ClientConfig{
+		TimeoutMs:            10 * 1000,
+		BeatInterval:         5 * 1000,
+		OpenKMS:              false,
+		CacheDir:             file.GetCurrentPath() + string(os.PathSeparator) + "cache",
+		UpdateThreadNum:      20,
+		NotLoadCacheAtStart:  true,
+		UpdateCacheWhenEmpty: false,
+		LogDir:               file.GetCurrentPath() + string(os.PathSeparator) + "log",
+		RotateTime:           "1h",
+		MaxAge:               3,
+		LogLevel:             "error",
+	}
+	for _, opt := range copts {
+		opt(cc)
+	}
 	client, err := clients.NewNamingClient(
 		vo.NacosClientParam{ClientConfig: cc, ServerConfigs: sc},
 	)
