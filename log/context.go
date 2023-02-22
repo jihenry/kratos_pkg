993113c9d2b0222700ca8f12145afa6b41bb3e59
wdoeasy <wdoeasy@126.com>
2023-02-22 18:32:16 +0800

    完全替换kratos的log

--- log/context.go
+++ log/context.go
@@ -13,16 +13,11 @@ func NewContext(ctx context.Context, logger log.Logger) context.Context {
 	return context.WithValue(ctx, loggerContextKey{}, logger)
 }
 
-func FromContext(ctx context.Context) *log.Helper {
-	logger := FromLogger(ctx)
-	return log.NewHelper(logger)
-}
-
-func FromLogger(ctx context.Context) log.Logger {
+func FromContext(ctx context.Context, opts ...log.Option) *log.Helper {
 	logger := log.GetLogger()
 	if ctxLogger, ok := ctx.Value(loggerContextKey{}).(log.Logger); ok {
 		logger = ctxLogger
 	}
 	ctxValueLogger := log.WithContext(ctx, logger)
-	return ctxValueLogger
+	return log.NewHelper(ctxValueLogger, opts...)
 }
