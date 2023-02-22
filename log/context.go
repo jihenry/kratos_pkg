2c95c8a93c23156b04e6ee075363cccc3534b2f4
wdoeasy <wdoeasy@126.com>
2023-02-22 18:36:34 +0800

    添加log扩展

--- log/context.go
+++ log/context.go
@@ -13,11 +13,14 @@ func NewContext(ctx context.Context, logger log.Logger) context.Context {
 	return context.WithValue(ctx, loggerContextKey{}, logger)
 }
 
-func FromContext(ctx context.Context, opts ...log.Option) *log.Helper {
+func FromContext(ctx context.Context, kvs ...interface{}) *log.Helper {
 	logger := log.GetLogger()
 	if ctxLogger, ok := ctx.Value(loggerContextKey{}).(log.Logger); ok {
 		logger = ctxLogger
 	}
 	ctxValueLogger := log.WithContext(ctx, logger)
-	return log.NewHelper(ctxValueLogger, opts...)
+	if len(kvs) > 0 {
+		ctxValueLogger = log.With(ctxValueLogger, kvs...)
+	}
+	return log.NewHelper(ctxValueLogger)
 }
